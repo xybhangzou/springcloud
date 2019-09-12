41f09a14f6e5c4fecf130372e380e7b8d3c99977
xyb <990945884@qq.com>
2019-09-12 15:50:44 +0800

    222

--- zuul/src/main/java/com/southwind/ZuulApplication.java
+++ zuul/src/main/java/com/southwind/ZuulApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableAutoConfiguration
 public class ZuulApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(ZuulApplication.class,args);
     }
 }
